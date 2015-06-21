6611b5982aad44567bbf8cbabb0c23a99a5e8b78
Jeremy Jack <jjack@mediatemple.net>
2015-06-20 20:24:13 -0700

    fleshing out the comments a little and updating some formatting

--- fancontrol.sh
+++ fancontrol.sh
@@ -2,14 +2,17 @@
 
 # OpenWRT fan control using RickStep's logic
 
+# set this to 1 for some debugging output
 VERBOSE=1
 
+# get some initial readings
 CPU_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon2/temp1_input`        
-DDR_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp1_input`   
+RAM_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp1_input`   
 WIFI_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp2_input`
+
 EMERGENCY_COOLDOWN=0
-EMERGENCY_COOLDOWN_TIMELEFT=0
-LOOP_COUNTER=1;
+EMERGENCY_COOLDOWN_TIMER=0
+LOOP_COUNTER=0
 
 # determine fan controller
 if [ -d /sys/devices/pwm_fan ]; then
@@ -20,34 +23,41 @@ else
     exit 0
 fi
 
-# simple function to make setting the fan a little nicer
-setFan() {
+# use this to make setting the fan a bit easier
+#     set_fan WHAT VALUE
+set_fan() {
     if [ $VERBOSE == 1 ]; then
-        echo "setFan: $1 ${FAN_CTRL}"
+        echo "setting fan to ${2} (${1}) ${FAN_CTRL}"
     fi
 
-    echo $1 > ${FAN_CTRL}
+    echo $2 > ${FAN_CTRL}
 }
 
-# floating-point greater-than-or-equals-to
-fge() {
-    awk -v n1=$1 -v n2=$2 'BEGIN{ if (n1>=n2) exit 1; exit 0}';
-    echo $?;
+# floating-point greater-than-or-equals-to using awk 'cause ash doesn't
+# like floats. instead of this:
+#     if [ $VALUE_1 >= $VALUE_2 ];
+# use this:
+#     if [ $(fge $VALUE_1 $VALUE_2) ];
+float_ge() {
+    awk -v n1=$1 -v n2=$2 "BEGIN { if ( n1 >= n2 ) exit 1; exit 0; }"
+    echo $?
 }
 
-# trigger the emergency cooldown mode
-startEmergencyCooldown() {
+# start the emergency cooldown mode
+start_emergency_cooldown() {
     if [ $VERBOSE == 1 ]; then
         echo "Starting Emergency Cooldown!"
     fi
 
-    EMERGENCY_COOLDOWN=1; 
-    EMERGENCY_COOLDOWN_TIMELEFT=30;
-    setFan 255;
+    # toggle the cooldown bit to on and reset the timer
+    EMERGENCY_COOLDOWN=1
+    EMERGENCY_COOLDOWN_TIMER=30
+
+    set_fan EMERGENCY 255
 }              
 
 # check for load averages above 1.0
-checkLoads() {
+check_load() {
     # loop over each load value (1 min, 5 min, 15 min)
     for LOAD in `cat /proc/loadavg | cut -d " " -f1,2,3`; do
         if [ $VERBOSE == 1 ]; then
@@ -55,87 +65,107 @@ checkLoads() {
         fi
 
         # trigger the emergency cooldown if we're using more than 1 core
-        if [ $(fge $LOAD 1.0) == 1 ]; then
-            startEmergencyCooldown;
+        if [ $(float_ge $LOAD 1.0) == 1 ]; then
+            start_emergency_cooldown
         fi
     done
 }
 
-# makes sure that the temperatures haven't fluctuated by more than 1 degrees
-checkTempDelta() {
-    TEMP_DELTA=$(($2 - $1));
+# makes sure that the temperatures haven't fluctuated by more than 1.5 degrees
+check_temp_delta() {
+    TEMP_DELTA=$(($3 - $2));
 
     if [ $VERBOSE == 1 ]; then
-        echo "Original Temp: $1 | New Temp: $2 | Delta: $TEMP_DELTA"
+        echo "${1} original temp: ${2} | new temp: ${3} | delta: ${TEMP_DELTA}"
     fi
 
-    if [ $(fge $TEMP_DELTA 1.5) == 1 ]; then
-       startEmergencyCooldown;
+    if [ $(float_ge $TEMP_DELTA 1.5) == 1 ]; then
+       start_emergency_cooldown;
     fi
 }
 
 # set fan speeds based on CPU temperatures
-checkCPUTemp() {
+check_cpu_temp() {
     if [ $VERBOSE == 1 ] ; then
         echo "Checking CPU Temp ${CPU_TEMP}"
     fi
 
     if [ $CPU_TEMP -ge 70 ]; then
-        setFan 255;
-    elif [ $(fge CPU_TEMP 67.5) ]; then
-        setFan 223;
+        set_fan CPU 255
+    elif [ $(float_ge CPU_TEMP 67.5) ]; then
+        set_fan CPU 223
     elif [ $CPU_TEMP -ge 65 ]; then
-        setFan 191;
-    elif [ $(fge $CPU_TEMP 62.5) ]; then
-        setFan 159;
+        set_fan CPU 191
+    elif [ $(float_ge $CPU_TEMP 62.5) ]; then
+        set_fan CPU 159
     elif [ $CPU_TEMP -ge 60 ]; then
-        setFan 127;
+        set_fan CPU 127
     elif [ $CPU_TEMP -ge 55 ]; then
-        setFan 95;
+        set_fan CPU 95
     elif [ $CPU_TEMP -ge 50 ]; then
-        setFan 63;
+        set_fan CPU 63
     fi
 }
 
-while true ; do
-    # save the previous temperatures
-    LAST_CPU_TEMP=$CPU_TEMP;
-    LAST_DDR_TEMP=$DDR_TEMP;
-    LAST_WIFI_TEMP=$WIFI_TEMP;
+# start the fan initially at 100
+set_fan START 100
 
-    # re-read the temps
+# the main program loop:
+# - look at load averages every 5 seconds
+# - look at temperature deltas every 5 seconds
+# - look at raw cpu temp every 20 seconds
+while true ; do
+    LOOP_COUNTER=$(($LOOP_COUNTER + 1));
+
+    # save the previous temperatures
+    LAST_CPU_TEMP=$CPU_TEMP
+    LAST_RAM_TEMP=$RAM_TEMP
+    LAST_WIFI_TEMP=$WIFI_TEMP
+
+    # and re-read the current temperatures
     CPU_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon2/temp1_input`
-    DDR_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp1_input`
+    RAM_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp1_input`
     WIFI_TEMP=`cut -c1-2 /sys/class/hwmon/hwmon1/temp2_input`
 
-    # check for the emergency cooldown mode
+    # handle emergency cooldown stuff
     if [ EMERGENCY_COOLDOWN == 1 ]; then
-        # reduce the number of seconds left
-        EMERGENCY_COOLDOWN_TIMELEFT=$(($EMERGENCY_COOLDOWN_TIMELEFT} - 5));
+
+        # reduce the number of seconds left in emergency cooldown mode
+        EMERGENCY_COOLDOWN_TIMER=$(($EMERGENCY_COOLDOWN_TIMER} - 5))
 
         # do we still need to be in cooldown?
-        if [ EMERGENCY_COOLDOWN_TIMELEFT <= 0]; then
-            EMERGENCY_COOLDOWN=0;
+        if [ $EMERGENCY_COOLDOWN_TIMER <= 0 ]; then
+
+            if [ $VERBOSE == 1 ]; then
+                echo "Exiting Emergency Cooldown Mode!"
+            fi
+
+            EMERGENCY_COOLDOWN=0
         else
-            sleep 5;
-            continue;
+            sleep 5
+
+            continue
         fi
     fi
 
-    # check the load averages > 1.0
-    checkLoads;
+    # check the load averages
+    check_load
 
-    # make sure that the CPU, DDR, and WiFI temps haven't spiked
-    checkTempDelta $CPU_TEMP $LAST_CPU_TEMP;
-    checkTempDelta $DDR_TEMP $LAST_DDR_TEMP;
-    checkTempDelta $WIFI_TEMP $LAST_WIFI_TEMP;
+    # check to see if the cpu, ram, or wifi temps have spiked
+    check_temp_delta CPU $CPU_TEMP $LAST_CPU_TEMP
+    check_temp_delta RAM $RAM_TEMP $LAST_RAM_TEMP
+    check_temp_delta WIFI $WIFI_TEMP $LAST_WIFI_TEMP
 
     # check the raw CPU temps every 20 seconds
     if [ $(($LOOP_COUNTER % 4)) == 0 ] ; then
-        checkCPUTemp;
+        check_cpu_temp;
     fi
 
     # wait 5 seconds and do this again
+    if [ $VERBOSE == 1 ]; then
+        echo "waiting 5 seconds..."
+        echo
+    fi
+
     sleep 5;
-    LOOP_COUNTER=$(($LOOP_COUNTER + 1));
 done
